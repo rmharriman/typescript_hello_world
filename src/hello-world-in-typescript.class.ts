39ffe953c75cd02ccc0adf27aa0508e98a78ce7a
Rob Harriman <robert.harriman@nih.gov>
2019-07-10 14:44:29 -0400

    Added first interface

--- src/hello-world-in-typescript.class.ts
+++ src/hello-world-in-typescript.class.ts
@@ -1,7 +1,46 @@
 export class HelloWorldInTypescript {
     static HelloWorld() {
+      
+      interface LabeledValue {
+        label: string
+      }
+  
+      function printLabel(labeledObj: LabeledValue) {
+        console.log(labeledObj.label);
+      }
+
+      let myObj = {size: 10, label: "Size 10 object"};
+      printLabel(myObj)
+
+
+      interface SquareConfig {
+        color?: string;
+        width?: number;
+      }
+
+      function createSquare(config: SquareConfig): {color: string; area: number} {
+
+        let newSquare = {color: "white", area: 1000}
+        if (config.color) {
+          newSquare.color = config.color;
+        }
+
+        if (config.width) {
+          newSquare.area = config.width * config.width;
+        }
+        return newSquare
+      }
+
+      let mySquare = createSquare({color: "black"});
+  
+      console.log(mySquare)
+
       let h1 = document.createElement("h1");
       h1.innerText = "Hello, World in Typescript!!";
       document.body.appendChild(h1);
+
+
+
+
     }
   }
