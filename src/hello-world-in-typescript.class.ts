c460600a21aa1b7f9413d79f1b15c625e0b2e1a9
Rob Harriman <robert.harriman@nih.gov>
2019-07-11 14:16:35 -0400

    Added interface examples

--- src/hello-world-in-typescript.class.ts
+++ src/hello-world-in-typescript.class.ts
@@ -1,6 +1,7 @@
 export class HelloWorldInTypescript {
     static HelloWorld() {
       
+      // Interface example for an object
       interface LabeledValue {
         label: string
       }
@@ -11,6 +12,37 @@ export class HelloWorldInTypescript {
 
       let myObj = {size: 10, label: "Size 10 object"};
       printLabel(myObj)
+
+      // Functions can have interfaces too
+      // Like a function declaration, but only parameter list and return type
+      interface SearchFunc {
+        (source: string, subString: string): boolean;
+      }
+      
+      // Create variable as function type (from interface)
+      let mySearch: SearchFunc;
+      // Assign new variable a function that respects contract
+      mySearch = function(source: string, subString: string) {
+        let result = source.search(subString);
+        return result > -1;
+      }
+    
+      // Can also use interfaces to describe types that can "index into"
+      // Add an index signature that describes the types to use to index (can index with number or string)
+      // Translation: When a StringArray is indexed with a number, it will return a string
+      interface StringArray {
+        [index: number]: string;
+      }
+
+      // Interfaces are commonly used to enforce that a class meets a particular contract
+      interface ClockInterface {
+        currentTime: Date;
+      }
+    
+      class Clock implements ClockInterface {
+        currentTime: Date = new Date();
+        constructor(h: number, m: number) { }
+      }
 
 
       interface SquareConfig {
