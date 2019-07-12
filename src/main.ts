92fbd4a90c6a1043410170b01ff10bac6d7793e1
Rob Harriman <robert.harriman@nih.gov>
2019-07-12 11:00:28 -0400

    Added another demo class

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { HelloWorldInTypescript } 
     from './hello-world-in-typescript.class';
 
+import { Grid } from './class-practice.class';
+
 const style = require("./main.css");
 
 HelloWorldInTypescript.HelloWorld();
+
+let grid1 = new Grid(1.0);  // 1x scale
+let grid2 = new Grid(5.0);  // 5x scale
+
+console.log(grid1.calculateDistanceFromOrigin({x: 10, y: 10}));
+console.log(grid2.calculateDistanceFromOrigin({x: 10, y: 10}));
