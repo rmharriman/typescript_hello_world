ddc04127ad6dac86e0449202d084e840b3bcf390
Rob Harriman <robert.harriman@nih.gov>
2019-07-12 10:55:53 -0400

    Added interface practice to hello class

--- src/hello-world-in-typescript.class.ts
+++ src/hello-world-in-typescript.class.ts
@@ -37,14 +37,21 @@ export class HelloWorldInTypescript {
       // Interfaces are commonly used to enforce that a class meets a particular contract
       interface ClockInterface {
         currentTime: Date;
+        setTime(d: Date): void;
       }
     
       class Clock implements ClockInterface {
         currentTime: Date = new Date();
+        setTime(d: Date) {
+          this.currentTime = d;
+        }
         constructor(h: number, m: number) { }
       }
+      
+      
 
 
+      //////////////////////////////////
       interface SquareConfig {
         color?: string;
         width?: number;
